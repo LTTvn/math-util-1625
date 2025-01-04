f9e2305a7f4229c9bde53cfd3f9446cc4a859968
LTTvn <lethitra2k4@gmail.com>
2025-01-04 21:51:05 +0700

    refact the main code and green 4ever always

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ### You will find in this report the following stuff:
 * The Math-Util library that offers multiple math function(a Java-based source code)
 * Unit Testing source code with JUnit framework
+* GitHub Actions to demonstrate the Continuous Integration(CI) technique
+* Regression Test
 *...
 
 ### Screenshots
--- src/com/tra/mathutil/core/MathUtil.java
+++ src/com/tra/mathutil/core/MathUtil.java
@@ -23,15 +23,38 @@ public class MathUtil {
         //20 giai thừa 18 con số 0 , vừa kịp dù cho kiểu long của Java
         //21 giai thừa tràn kiểu long
         //bài này quy ước tính n! trong khoảng từ 0..20
+//        public static long getFactorial (int n){
+//             if(n<0 || n>20)
+//                 throw new IllegalArgumentException("Invalid argument.N must be between 0 .. 20");
+//             if(n==0 || n==1)
+//                 return 1; //kết thúc cuộc chơi sớm nêu nhận những đầu vào đặc biệt
+//             long product =1 ; // tích nhân dồn,thuật toán heo đát ,ốc bu dồn thịt
+//             for (int i = 2; i <= n; i++) 
+//                 product *= i; // product = product * i
+//             return product;
+//            }
+        
+        //Hocj đệ quy trong vòng 30s - RECURSION
+        //Hiện tượng gọi lại chính mình với 1 quy mô khác
+        //Ví dụ: con búp bê Nga, giống nhau và lồng trong nhau 
+        // búp bê to , nhỏ hơn , nhỏ hơn nữa ,... đến điểm dừng 
+        
+        //tính giùm tui 6!
+        //n!=1.2.3.4.5.6. ... .n
+        //6! = 6 X 5!
+        //5! = 5 X 4!
+        //4!= 4 X 3!
+        //3!= 3 X 2!
+        //2! = 2 X 1! // điểm dừng 
+        //1!=1;
+        
+        //quy ước 1! = 0! = 1;
+        // chốt lại : n! = n * (n-1)!
+        
         public static long getFactorial (int n){
-             if(n<0 || n>20)
-                 throw new IllegalArgumentException("Invalid argument.N must be between 0 .. 20");
              if(n==0 || n==1)
                  return 1; //kết thúc cuộc chơi sớm nêu nhận những đầu vào đặc biệt
-             long product =1 ; // tích nhân dồn,thuật toán heo đát ,ốc bu dồn thịt
-             for (int i = 2; i <= n; i++) 
-                 product *= i; // product = product * i
-             return product;
+             return n * getFactorial(n-1);// công thức đệ quy 
             }
         }
          
